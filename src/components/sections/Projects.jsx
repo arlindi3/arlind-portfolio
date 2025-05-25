3a8833e682bd01a61bf48e096cff41e25108528a
arlindi3 <draboarlind@gmail.com>
2025-05-25 13:37:29 +0200

    Initial commit

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -39,7 +39,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://arlindi3.github.io/stagged-chess-game/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project ➡
@@ -47,13 +47,12 @@ export const Projects = () => {
               </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59.130.246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2">Staged Chess Game</h3>
+              <h3 className="text-xl font-bold mb-2">Bar App</h3>
               <p className="text-gray-400 mb-4">
-                Simple stage game with some buttons and a board that shows the
-                next move
+                Simple bar application with React components
               </p>
               <div>
-                {["Html", "Css", "JavaScript"].map((tech, key) => (
+                {["Html", "Css", "JavaScript", "React js"].map((tech, key) => (
                   <span
                     key={tech}
                     className="bg-blue-500/10 text-blue-500 py-1 px-2 rounded-full text-sm hover:bg-blur-500/20 hover:shadow-[0_2px_8px_rgba(59.130.246,0.1)] transition-all"
@@ -64,7 +63,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://arlindi3.github.io/bar-app"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project ➡
@@ -72,13 +71,13 @@ export const Projects = () => {
               </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59.130.246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2">Staged Chess Game</h3>
+              <h3 className="text-xl font-bold mb-2">Ios Dashboard Example</h3>
               <p className="text-gray-400 mb-4">
-                Simple stage game with some buttons and a board that shows the
-                next move
+                Simple Ios Dashboard Example with some applications made with
+                React components
               </p>
               <div>
-                {["Html", "Css", "JavaScript"].map((tech, key) => (
+                {["Html", "Css", "JavaScript", "React js"].map((tech, key) => (
                   <span
                     key={tech}
                     className="bg-blue-500/10 text-blue-500 py-1 px-2 rounded-full text-sm hover:bg-blur-500/20 hover:shadow-[0_2px_8px_rgba(59.130.246,0.1)] transition-all"
@@ -89,7 +88,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://arlindi3.github.io/dashboard-example"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project ➡
